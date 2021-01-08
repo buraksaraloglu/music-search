98c9dec677e7e463d900ef58a0005e1f0f1e23a8
Burak Saraloglu <buraksaraloglu1@gmail.com>
2021-01-08 10:29:52 +0300

    Fetch Musics API

--- src/hooks/searchFetch.js
+++ src/hooks/searchFetch.js
@@ -34,7 +34,9 @@ export const searchFetch = (query) => {
 	useEffect(() => {
 		if (query !== undefined && authToken !== undefined) {
 			let response = searchMusic(query, authToken);
+			setContentStorage(response.musics);
 		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [query]);
 };
 
