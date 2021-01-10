0942c429085772e8740657ecbc5c8579a1a4f6b0
Burak Saraloglu <buraksaraloglu1@gmail.com>
2021-01-10 07:03:51 +0300

    Token error caused by api server

--- src/hooks/setClientToken.js
+++ src/hooks/setClientToken.js
@@ -1,19 +1,25 @@
-import axios from 'axios';
+// import axios from 'axios';
 
 const setClientToken = () => {
-	// const responseToken = '1.YnVyYWtzYXJhbG9nbHU=.VLeJw80DMpXMxxgUJWW3Nrj1';
 	return new Promise((resolveResponse, rejectResponse) => {
+		const responseToken = '1.YnVyYWtzYXJhbG9nbHU=.VLeJw80DMpXMxxgUJWW3Nrj1';
 		try {
-			axios
-				.post('https://musicdb.jobs.otsimo.com/createClient', {
-					email: 'buraksaraloglu1@gmail.com',
-				})
-				.then((res) => {
-					resolveResponse(res.data);
-				})
-				.catch((error) => {
-					rejectResponse(console.error(error));
-				});
+			// axios
+			// 	.post('https://musicdb.jobs.otsimo.com/createClient', {
+			// 		email: 'buraksaraloglu1@gmail.com',
+			// 	})
+			// 	.then((res) => {
+			// 		resolveResponse(res.data);
+			// 	})
+			// 	.catch((error) => {
+			// 		rejectResponse(console.error(error));
+			// 	});
+
+			if (responseToken.length > 0) {
+				resolveResponse(responseToken);
+			} else {
+				rejectResponse('Token not found.');
+			}
 		} catch (error) {
 			throw new Error(error);
 		}
